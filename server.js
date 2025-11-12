850dd4f67a80c4a0058313e663ed469429968951
ashmit65 <146719150+ashmit65@users.noreply.github.com>
2025-11-12 23:56:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
-app.use(express.static("public")); // change to your folder name if needed
+app.use(express.static("views")); // change to your folder name if needed
 
 const server = http.createServer(app);
 
